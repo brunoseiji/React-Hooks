b3fb8fefe37ce313e55aa29ce0eeb45c21c49aad
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2020-04-01 19:24:55 -0300

    React Hooks do curso de React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas');
+
+    if(tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas])
+
+  const handleAdd = useCallback(() => { 
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }, [input, tarefas]);
+
+  // Só é chamada quando possui alterações na state [tarefas]
+  const totalTarefas = useMemo( () => tarefas.length, [tarefas] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <br/>
+        <strong>Você tem {totalTarefas} tarefas!</strong>
+      <br/>
+      <br/>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+
+      <button type="button" onClick={handleAdd}>Adicionar Tarefa</button>
+
     </div>
   );
 }
